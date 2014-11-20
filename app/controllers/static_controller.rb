a1f20f672aaf1ea8dd61eb8ca738b48f6c58bc5e
zubaird <zubair.desai@gmail.com>
2014-11-19 17:50:40 -0800

    Add quotes on front page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,19 @@
 class StaticController < ApplicationController
 
   def home
+    @quotes = [
+"'Failure is not an option. Everyone has to Succeed' - Arnold Schwarzenegger",
+"'Your time is limited, so don't waste it living someone else's life.' - Steve Jobs",
+"'Better Ingredients, Better Pizza' - Papa John",
+]
+  end
+
+  def terms
 
   end
-  
+
+  def about
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root 'static#home'
 
+  get 'about' => "static#about"
+  get 'terms' => "static#terms"
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
